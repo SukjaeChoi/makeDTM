7f3f25b5e8a985506d363e5c069c68f5b81e9f9e
SukjaeChoi <lingua@daum.net>
2018-08-26 00:58:44 +0900

    Add files via upload

--- R/makeDTM.R
+++ R/makeDTM.R
@@ -8,17 +8,119 @@
 #' @param weight character. tf or tfidf
 #' @param TEXT.name character. Column name which whould be TEXT.
 #' @param LABEL.name character. Column name which would be LABEL.
-#' @param RHINO logical. In case of morphological anaysis need for TEXT column.
+#' @param RHINO logical. In need of morphological anaysis about TEXT column.
 #' @param pos character. Among Right: ALL(all Part-Of-Speech), noun(NNG, NNP, NP), verb(VV, VA, XR), NNG, NNP, NP, NNB, VV, VA, XR, VX
 #' @return matrix or dataframe(LABEL=TRUE)
 #' @export
 #' @examples
 #' makeDTM(docs = docs, key = c("excel", "computer"), pos="noun")
 #' makeDTM(docs = docs, key = c("excel", "computer"), LABEL = TRUE, weight = "tfidf", TEXT.name="body", LABEL.name="tag", pos="verb")
+#' makeDTM(docs = docs, key = c("excel", "computer"), weight = "tfidf", TEXT.name="body", LABEL.name="tag", pos="verb")
 
 makeDTM <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL, LABEL.name=NULL, RHINO=FALSE, pos="ALL")
 {
-  # 컬럼의 이름을 바꾸기
+  # Change column name
+  doc.col <- colnames(docs)
+  if(!is.null(TEXT.name))
+    doc.col <- replace(doc.col, doc.col==TEXT.name, "TEXT")
+  if(!is.null(LABEL.name))
+    doc.col <- replace(doc.col, doc.col==LABEL.name, "LABEL")
+  colnames(docs) <- doc.col
+  
+  if(!any(grepl("TEXT", colnames(docs))))
+    print("Please use TEXT.name argument")
+  if(LABEL==TRUE||!is.null(LABEL.name))
+  {
+    if(!any(grepl("LABEL", colnames(docs))))
+      print("Please use LABEL.name argument")
+  }
+  
+  docs$TEXT <- as.character(docs$TEXT)
+  
+  # Morphological Analysis for TEXT column
+  if(RHINO)
+  {
+    library(RHINO)
+    #initRhino()
+    docs$TEXT <- lapply(docs$TEXT, getMorph, pos)
+    for(i in 1:length(docs$TEXT))
+      docs$TEXT[i] <- paste(docs$TEXT[[i]], collapse = " ")
+    docs$TEXT <- as.character(docs$TEXT)
+  }
+  
+  # If not set for keyword, make every word as keyword
+  if(length(key)==1)
+  {
+    if(key[1] == "all")
+      key <- unique(unlist(strsplit(docs$TEXT, split = " ")))
+  }
+  
+  # Calculate tf or tf-idf
+  dtm <- matrix(, nrow = NROW(docs), ncol = length(key))          # null matrix for final result
+  dtm.inter <- matrix(, nrow = NROW(docs), ncol = length(key))    # matrix for idf 
+  
+  if(weight=="tf"){
+    
+    for(i in 1:NROW(docs))
+    {
+      docwords <- unlist(strsplit(docs$TEXT[i], split = " "))
+      dtm[i,] <- table(docwords[which(docwords %in% key)])[key]
+    }
+    
+    rownames(dtm) <- 1:NROW(docs)
+    colnames(dtm) <- key
+    dtm[is.na(dtm)] <- 0    # Change every NA to 0 
+    
+    
+  }else if(weight=="tfidf"){
+    
+    # Calculate tf
+    for(i in 1:NROW(docs)) {
+      docwords <- unlist(strsplit(docs$TEXT[i], split = " "))
+      dtm[i,] <- table(docwords[which(docwords %in% key)])[key]
+    }
+    
+    rownames(dtm) <- 1:NROW(docs)
+    colnames(dtm) <- key
+    dtm[is.na(dtm)] <- 0    # Change every NA to 0 
+    
+    # Calculate idf
+    for(i in 1:NROW(docs)) {
+      inter <- intersect(unlist(strsplit(docs$TEXT[i], split = " ")), key)  # intersection
+      dtm.inter[i,] <- table(inter[which(inter %in% key)])[key]
+    }
+
+    rownames(dtm.inter) <- 1:NROW(docs)
+    colnames(dtm.inter) <- key
+    dtm.inter[is.na(dtm.inter)] <- 0
+    
+    dtm.inter.df <- as.data.frame(dtm.inter)
+    doc_word_freq <- sapply(dtm.inter.df, sum)   # The number of documents which has term t
+    
+    idf <- log(NROW(docs) / doc_word_freq)       # idf value
+    idf[is.infinite(idf)] <- 0
+    
+    for(i in 1:ncol(dtm))
+      dtm[,i] <- dtm[,i] * idf[i]                # tf * idf
+  }
+  
+  if(LABEL||!is.null(LABEL.name))                # Make dataframe first, then paste LABEL
+  {
+    dtm <- as.data.frame(dtm)
+    dtm <- cbind(dtm, LABEL=docs$LABEL)
+  }
+  
+  return(dtm)
+}
+
+
+
+# This code is just for saving
+# This code speed is very slow 
+'
+makeDTM_origin <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL, LABEL.name=NULL, RHINO=FALSE, pos="ALL")
+{
+  # Change Column name
   doc.col <- colnames(docs)
   if(!is.null(TEXT.name))
     doc.col <- replace(doc.col, doc.col==TEXT.name, "TEXT")
@@ -36,7 +138,7 @@ makeDTM <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL,
 
   docs$TEXT <- as.character(docs$TEXT)
 
-  # TEXT 컬럼에 대한 형태소 분석
+  # Morphological analysis for TEXT column
   if(RHINO)
   {
     library(RHINO)
@@ -47,14 +149,14 @@ makeDTM <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL,
     docs$TEXT <- as.character(docs$TEXT)
   }
 
-  # keyword 설정이 없는 경우, 모든 단어를 keyword로 삼기
+  # If not set for keyword, make every word as keyword
   if(length(key)==1)
   {
     if(key[1] == "all")
       key <- unique(unlist(strsplit(docs$TEXT, split = " ")))
   }
 
-  # tf 또는 tf-idf 값 구하기
+  # Calculate tf or tf-idf
   dtm <- c()
   if(weight=="tf"){
     for(i in 1:nrow(docs)){
@@ -68,18 +170,18 @@ makeDTM <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL,
     dtm[is.na(dtm)] <- 0
   }else if(weight=="tfidf"){
 
-    # tf 구하기
+    # Calculate tf
     for(i in 1:nrow(docs)){
       docwords <- unlist(strsplit(docs$TEXT[i], split = " "))
       i_word <- docwords[which(docwords %in% key)]
       dtm <- rbind(dtm, table(i_word)[key])
     }
 
-    # idf 구하기
+    # Calculate idf
     contents <- c()
     for(i in 1:nrow(docs))
     {
-      inter <- intersect(unlist(strsplit(docs$TEXT[i], split = " ")), key)  # 교집합
+      inter <- intersect(unlist(strsplit(docs$TEXT[i], split = " ")), key)  # intersection
       contents <- rbind(contents, table(inter)[key])
     }
 
@@ -92,16 +194,16 @@ makeDTM <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL,
     contents[is.na(contents)] <- 0
 
     contents.df <- as.data.frame(contents)
-    doc_word_freq <- sapply(contents.df, sum)   # t가 나오는 문서의 수
+    doc_word_freq <- sapply(contents.df, sum)   # The number of documents which has term t
 
-    idf <- log(nrow(docs) / doc_word_freq)      # idf 값
+    idf <- log(nrow(docs) / doc_word_freq)      # idf value
     idf[is.infinite(idf)] <- 0
 
     for(i in 1:ncol(dtm))
       dtm[,i] <- dtm[,i] * idf[i]               # tf * idf
   }
 
-  if(LABEL)             # 먼저 데이터프레임으로 만들고, LABEL을 붙인다
+  if(LABEL)             # Make dataframe first, then paste LABEL
   {
     dtm <- as.data.frame(dtm)
     dtm <- cbind(dtm, LABEL=docs$LABEL)
@@ -109,7 +211,7 @@ makeDTM <- function(docs, key="all", LABEL=FALSE, weight = "tf", TEXT.name=NULL,
 
   return(dtm)
 }
-
+'
 
 
 #' findAssocTwo
